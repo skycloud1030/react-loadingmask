6303e791e7085a7e226df19d2e8daf8b568c340e
CT Hung <skycloud1030@gmail.com>
2018-08-17 10:24:42 +0800

    Add bootstrap to demo

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import LoadingMask from "react-loadingmask";
 import "react-loadingmask/dist/react-loadingmask.css";
 import "./index.css"; // User Customize
 
-const Spin = <i className="fas fa-sync-alt fa-spin" style={{ fontSize: 24 }} />;
-const margin = { marginBottom: 20 };
+/**
+ * Font awesonme Component
+ */
+const Icon = ({ type, className = "", style = {} } = {}) => (
+  <i className={`fas fa-${type} ${className}`} style={style} />
+);
 
-class Demo extends React.Component {
+const fontSize = 24;
+const Spin = <i className="fas fa-sync-alt rc-loading-spin" style={{ fontSize }} />;
+const Rocket = <i className="fas fa-rocket rc-loading-move-right" style={{ fontSize, color: "red" }} />;
+const RocketHor = <Icon type="rocket" className="animation-horizon" style={{ fontSize, color: "#120338" }} />;
+const styles = { marginBottom: 20, height: 150 };
+
+class Demo extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -20,20 +30,63 @@ class Demo extends React.Component {
   render() {
     const { loading } = this.state;
     return (
-      <div>
-        <button onClick={this.onLoadingSwitch} style={margin}>
-          {loading ? "Close Loading" : "Open Loading"}
-        </button>
-        <LoadingMask loading={loading} loadingText={"Default"}>
-          <div style={{ ...margin, width: 500, height: 300 }}>Compoment You want to display</div>
-        </LoadingMask>
-
-        <LoadingMask loading={true} loadingText={"Customize"} indicator={Spin} className="customize">
-          <div style={{ width: 500, height: 300 }}>Customize with font awesome</div>
-        </LoadingMask>
+      <div className="container">
+        <Row>
+          <Col span={6} style={{ marginTop: 20 }}>
+            <Button style={{ marginBottom: 20 }} bsStyle="primary" onClick={this.onLoadingSwitch}>
+              {loading ? "Close Loading" : "Open Loading"}
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={6}>
+            <LoadingMask loading={loading} loadingText={"Default"}>
+              <div style={styles}>Compoment You want to display</div>
+            </LoadingMask>
+          </Col>
+          <Col span={6}>
+            <LoadingMask className="customize" loading={loading} loadingText={"Customize"} indicator={Spin}>
+              <div style={styles}>Customize with font awesome</div>
+            </LoadingMask>
+          </Col>
+          <Col span={12}>
+            <LoadingMask loading={loading} loadingText={"More animation"} indicator={Rocket}>
+              <div style={styles}>More animation, Control by CSS</div>
+            </LoadingMask>
+          </Col>
+          <Col span={12}>
+            <LoadingMask
+              className="customize"
+              loading={loading}
+              loadingText={"Customize animation"}
+              indicator={RocketHor}
+            >
+              <div style={styles}>Your own animation</div>
+            </LoadingMask>
+          </Col>
+        </Row>
       </div>
     );
   }
 }
+
+/**
+ *  Boostrap Component
+ */
+const Row = props => <div className="row">{props.children}</div>;
+const Col = props => (
+  <div className={`col-${props.span}`} style={props.style}>
+    {props.children}
+  </div>
+);
+const Button = props => {
+  const { style, bsStyle, onClick } = props;
+  const className = bsStyle ? `btn btn-${bsStyle}` : "btn";
+  return (
+    <button style={style} className={className} onClick={onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 ReactDOM.render(<Demo />, document.getElementById("index"));
